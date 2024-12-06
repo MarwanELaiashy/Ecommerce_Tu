300b338dbd2c5608a0407923f3d1eca2704a9514
Marwan <marwanelaiashy1@gmail.com>
2024-12-06 22:23:43 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// set the date we're counting down to
+
+const countDownDate = new Date("Dec 5, 2025 16:27:25").getTime();
+
+// update the countdown every 1 second
+
+const x = setInterval(() => {
+  const now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+
+  // time calculations for days, hours, minutes, seconds
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) // distance % (1000 * 60 * 60 * 24) we use moduls to calculate the remainder after extracting the days
+  );
+
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // if the countdown finished , write Expired
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+// mobile nav
+
+const hamburger = document.querySelector(".hamburger");
+const Nav = document.querySelector(".mobile_nav");
+
+hamburger.addEventListener("click", () => {
+  Nav.classList.toggle("mobile_nav_hide");
+});
