603009dfe450e760ebba9b50dab8eb768429f2f8
Marwan <marwanelaiashy1@gmail.com>
2024-12-11 14:24:24 +0200

    Finish website and uploade it

--- js/app.js
+++ js/app.js
@@ -82,3 +82,37 @@ AddToCart.forEach((button) => {
     localStorage.setItem("cart", JSON.stringify(cart)); //LocalStorage must store values as string
   });
 });
+
+// Animations
+ScrollReveal().reveal(".top_nav", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+});
+ScrollReveal().reveal(".nav", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".header", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  delay: 200,
+});
+ScrollReveal().reveal(".section", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  duration: 1000,
+  delay: 100,
+});
+ScrollReveal().reveal(".footer", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  duration: 1000,
+  delay: 100,
+});
---
+++ js/signup.js
@@ -0,0 +1,8 @@
+// mobile nav
+
+const hamburger = document.querySelector(".hamburger");
+const Nav = document.querySelector(".mobile_nav");
+
+hamburger.addEventListener("click", () => {
+  Nav.classList.toggle("mobile_nav_hide");
+});
